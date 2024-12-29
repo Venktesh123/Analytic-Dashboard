e233853a1413287356e4da22995501cc18d42aa1
Venktesh123 <8303531530venktesh@gmail.com>
2024-12-29 11:03:34 +0530

    repository-setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const connectDB = require("./config/dbConnnection");
+require("dotenv").config();
+const weatherRouter = require("./router/weatherRouter");
+const stockRouter = require("./router/finanaceRouter");
+const authRouter = require("./router/authRouter");
+const stockmodificationRouter = require("./router/stockmodifyingRouter");
+const weathermodificationRouter = require("./router/weatherModifyingRouter");
+const authMiddleware = require("./middleware/authMiddleware");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+connectDB();
+app.get("/", (req, res) => {
+  res.send("Backend Working ON Port 3000");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
